db817244657b6fa58e10e03f2d6b53c3164fc339
Gabriel Erazo <gfranciscoerazom@protonmail.com>
2023-06-18 11:43:05 -0500

    Uses the connection to /api/Usuarios, performs a successful login and redirects to the Usuario/Index view.

---
+++ Controllers/UsuarioController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebPetShop.Models.APITTHH;
+using WebPetShop.Services;
+
+namespace WebPetShop.Controllers;
+
+public class UsuarioController : Controller
+{
+    private readonly IService_API _service;
+
+    public UsuarioController(IService_API service)
+    {
+        _service = service;
+    }
+
+    public async Task<ActionResult> Index()
+    {
+        var usuarioOutput = await _service.GetUsuarioOutputsAsync(new Usuario
+        {
+            usuario = "5005",
+            password = "5005U"
+        });
+
+        return View(usuarioOutput);
+    }
+}
